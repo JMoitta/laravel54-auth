7510bc7bb4519e4e92886ad7dd3623f61d5801da
mex8063 <mex8063@24046-5636.INFO>
2019-04-30 11:18:22 -0300

    Autorização

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\User;
+
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -25,6 +27,14 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::before(function($user){
+            if($user->role == User::ROLE_ADMIN){
+                return true;
+            }
+        });
+
+        /*Gate::define('admin', function($user){
+            return $user->role == User::ROLE_ADMIN;
+        });*/
     }
 }
